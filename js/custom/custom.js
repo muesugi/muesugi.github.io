61c16cbe726d2545e0478d879cf1f762db206f42
muesugi <moie.uesugi@gmail.com>
2016-01-12 15:48:22 -0500

    changed file architecture and added template files (and created resume and portfolio htmls

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -1,5 +1,4 @@
 window.addEventListener('resize', function(event){
-	landing_photo_resize();
 	if (window.innerWidth > 760){ /*not mobile*/
 		$.sidr('close', 'sidr');
 	}
@@ -11,18 +10,7 @@ $(document).ready(function() {
   $('#simple-menu').sidr({
       side: 'right'
     });
-  landing_photo_resize();
 });
-
-function landing_photo_resize(){
-	if (window.innerWidth <= 760){ /*mobile*/
-		$(".landing").width(window.innerWidth - 0.05*window.innerWidth);
-		$(".landing").css("padding-right", "5%");
-	}	
-	else{ /*not mobile*/
-	  $(".landing").width(window.innerWidth - $("#photo").width()- 0.05*window.innerWidth);
-	}
-}
 
 function create_sidr(){
 	var ulist = "<ul>";
---
+++ js/custom/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('resize', function(event){
+	landing_photo_resize();
+});
+$(document).ready(function() {
+  	landing_photo_resize();
+});
+
+function landing_photo_resize(){
+	if (window.innerWidth <= 760){ /*mobile*/
+		$(".landing").width(window.innerWidth - 0.05*window.innerWidth);
+		$(".landing").css("padding-right", "5%");
+	}	
+	else{ /*not mobile*/
+	  $(".landing").width(window.innerWidth - $("#photo").width()- 0.05*window.innerWidth);
+	}
+}
