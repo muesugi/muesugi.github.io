ff4684c79e16c2d1ac6cd41400bff61412090dc3
muesugi <moie.uesugi@gmail.com>
2017-08-12 15:06:23 -0400

    responsive navigation, hamburger menu

--- src/js/components/MainNavigation.js
+++ src/js/components/MainNavigation.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import ProfileImage from './ProfileImage';
+import { slide as Menu } from 'react-burger-menu';
+
 
 export default class MainNavigation extends React.Component {
+
   render(){
     return (
       <div className="main-nav">
-        <Link to='/' className="nav-left">
+        <Link to='/' className="nav-left desktop-nav">
           <ProfileImage width="50" />
           <span className="name">Moie Uesugi</span>
         </Link>
-        <nav className="nav-right">
+        <nav className="nav-right desktop-nav">
           <Link to='/'>about</Link>
           <NavLink to='/resume'>resume</NavLink>
           <NavLink to='/portfolio'>portfolio</NavLink>
           <NavLink to='/contact'>contact</NavLink>
         </nav>
+        <div id="hamburger-container">
+          <Menu right width="250">
+            <Link to='/'>about</Link>
+            <NavLink to='/resume'>resume</NavLink>
+            <NavLink to='/portfolio'>portfolio</NavLink>
+            <NavLink to='/contact'>contact</NavLink>
+          </Menu>
+        </div>
       </div>
     );
   }
