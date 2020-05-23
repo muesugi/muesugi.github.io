6e49c304dad1c3a322d063fb88e7daf2627ebf87
muesugi <moie.uesugi@gmail.com>
2020-05-23 13:21:18 -0700

    perf improvements - make heavy page level components loadable and remove unused assets

--- src/js/client.js
+++ src/js/client.js
@@ -1,36 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Switch, Route, Link } from "react-router-dom";
+import Loadable from 'react-loadable';
+
 import LandingPage from "./pages/LandingPage";
-import ResumePage from "./pages/ResumePage";
-import PortfolioPage from "./pages/PortfolioPage";
-import UIPortfolioPage from "./uiportfolio/UIPortfolioPage";
 import ContactPage from "./pages/ContactPage";
 
-import AdaptTranslations from "./uiportfolio/portfolio-entries/AdaptTranslations";
-import UnoPortInn from "./uiportfolio/portfolio-entries/UnoPortInn";
-import UnoPortArtFilms from "./uiportfolio/portfolio-entries/UnoPortArtFilms";
-import Greo from "./uiportfolio/portfolio-entries/Greo";
+export const Loading = () => (
+  <div></div>
+);
+
+const ResumePageLoadable = Loadable({
+  loader: () => import('./pages/ResumePage'),
+  loading: Loading,
+});
+
+const PortfolioPageLoadable = Loadable({
+  loader: () => import('./pages/PortfolioPage'),
+  loading: Loading,
+});
+
+
+const UIPortfolioPageLoadable = Loadable({
+  loader: () => import('./uiportfolio/UIPortfolioPage'),
+  loading: Loading,
+});
+
+const AdaptTranslationsLoadable = Loadable({
+  loader: () => import('./uiportfolio/portfolio-entries/AdaptTranslations'),
+  loading: Loading,
+});
+
+const UnoPortInnLoadable = Loadable({
+  loader: () => import('./uiportfolio/portfolio-entries/UnoPortInn'),
+  loading: Loading,
+});
+
+const UnoPortArtFilmsLoadable = Loadable({
+  loader: () => import('./uiportfolio/portfolio-entries/UnoPortArtFilms'),
+  loading: Loading,
+});
+
+const GreoLoadable = Loadable({
+  loader: () => import('./uiportfolio/portfolio-entries/Greo'),
+  loading: Loading,
+});
+
 
 ReactDOM.render(
   <HashRouter>
     <main>
       <Switch onUpdate={() => window.scrollTo(0, 0)}>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/resume" component={ResumePage} />
-        <Route path="/portfolio" component={PortfolioPage} />
-        <Route exact path="/ui-portfolio" component={UIPortfolioPage} />
+        <Route path="/resume" component={ResumePageLoadable} />
+        <Route path="/portfolio" component={PortfolioPageLoadable} />
+        <Route path="/contact" component={ContactPage} />
+
+        <Route exact path="/ui-portfolio" component={UIPortfolioPageLoadable} />
         <Route
           path="/ui-portfolio/adapt-translations"
-          component={AdaptTranslations}
+          component={AdaptTranslationsLoadable}
         />
-        <Route path="/ui-portfolio/unoportinn" component={UnoPortInn} />
+        <Route path="/ui-portfolio/unoportinn" component={UnoPortInnLoadable} />
         <Route
           path="/ui-portfolio/unoportartfilms"
-          component={UnoPortArtFilms}
+          component={UnoPortArtFilmsLoadable}
         />
-        <Route path="/ui-portfolio/greo" component={Greo} />
-        <Route path="/contact" component={ContactPage} />
+        <Route path="/ui-portfolio/greo" component={GreoLoadable} />
       </Switch>
     </main>
   </HashRouter>,
---
+++ src/js/helpers/loadable.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Loadable from 'react-loadable';
+
+export const Loading = () => (
+  <div></div>
+);
